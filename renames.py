668ee0687974ef1dc992b006719d81a0b0b24cca
iCoACN <38726127+iCoACN@users.noreply.github.com>
2020-01-15 11:18:31 +0800

    Add files via upload

---
+++ renames.py
@@ -0,0 +1,9 @@
+import os
+old = input('请输入要替换的内容：')
+new = input('请输入替换后的内容：')
+for file in os.listdir():
+	f_name = file
+	if f_name.count(old) and not os.path.isdir(file):
+		f_new = f_name.replace(old,new)
+		os.rename(f_name,f_new)
+		print(f_name,'>',f_new)
